4fb4e6687fc0ce0190cb9617b93de94b480193df
Icradin <aleksterziev@gmail.com>
2016-02-21 16:34:02 +0100

    Graphics changes

--- Corporate Game/Assets/Custom Assets/Scripts/transition.cs
+++ Corporate Game/Assets/Custom Assets/Scripts/transition.cs
@@ -7,14 +7,39 @@ public class transition : MonoBehaviour {
 
 	public int transition_number;
 
+    bool has_entered;
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (has_entered)
+            {
+                transition_manager.instance.transition(transition_number, gameObject);
+                //print("");
+                has_entered = false;
+
+            }
+           
+        }
+
+        //Debug.Log("has_entered   " + has_entered);
+    }
 
 
-	void OnTriggerStay(){
-		if (Input.GetKeyDown (KeyCode.E)) {
-            print("kk");
-            transition_manager.instance.transition (transition_number, gameObject);	
-		}
-			
-
+	void OnTriggerEnter(Collider player){
+		if (player.transform.CompareTag("player"))
+        {
+            has_entered = true;
+        }
 	}
+
+    void OnTriggerExit(Collider player)
+    {
+        if (player.transform.CompareTag("player"))
+        {
+            has_entered = false;
+        }
+    }
 }
